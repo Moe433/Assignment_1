c636eca81f024b70f30c9ecbeb2ba43ce029e031
Moe433 <mohammedqudaih22@gmail.com>
2025-02-19 20:31:08 -0500

    second update check

--- Test1.java
+++ Test1.java
@@ -1,6 +1,6 @@
 class Test1 {
 	public static void main(String[] args) {
-	System.out.println("Hello World!/n");
+	System.out.println("Hello World!/n\n");
 	System.out.println("Test Success");
 }
 }
